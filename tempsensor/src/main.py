6ca4042939e66d24c9c497a7909f754f5a3ee11b
Ludde <luddenorin@gmail.com>
2025-03-18 20:30:31 +0100

    tempsensor/src/main.py: tempsensor with onewire done

---
+++ tempsensor/src/main.py
@@ -0,0 +1,20 @@
+import onewire
+import machine
+import ds18x20
+import time
+
+#initialize pin connected to the ds sensor. 
+ds_pin = machine.Pin(0)
+#create a object from onewire lib initialize with the pin.
+ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
+
+#scan for the device on the bus.
+roms = ds_sensor.scan()
+
+while True :
+    ds_sensor.convert_temp()
+    time.sleep(250)
+    for rom in roms:
+        print(round(ds_sensor.read_temp(rom)), end= '\n')
+
+
