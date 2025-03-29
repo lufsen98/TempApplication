2ab86496697cbc781bda32736bbd6368e1b3e399
Ludde <luddenorin@gmail.com>
2025-03-29 10:45:56 +0100

    tempsensor py

---
+++ broker/notes/structure.md
@@ -0,0 +1,13 @@
+# MQTT-BROKER
+# Programming language 
+python or c/c++?
+# libraries 
+asyncio
+
+
+# components
+ios standard for the MQTT-BROKER what components do i need?
+
+## ports
+Port for mqtt is server 1883
+
--- tempsensor/src/main.py
+++ tempsensor/src/main.py
@@ -4,7 +4,7 @@ import ds18x20
 import time
 
 #initialize pin connected to the ds sensor. 
-ds_pin = machine.Pin(0)
+ds_pin = machine.Pin(12)
 #create a object from onewire lib initialize with the pin.
 ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
 
@@ -13,7 +13,7 @@ roms = ds_sensor.scan()
 
 while True :
     ds_sensor.convert_temp()
-    time.sleep(250)
+    time.sleep(1)
     for rom in roms:
         print(round(ds_sensor.read_temp(rom)), end= '\n')
 
